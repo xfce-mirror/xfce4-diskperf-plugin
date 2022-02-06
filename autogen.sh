4304489edd0e1bb09ee846c802a2f028f0dcc933
Jan Ziak <0xe2.0x9a.0x9b@xfce.org>
2022-02-07 00:04:19 +0100

    Update configuration files
    Also bump required Xfce libraries from version 4.12 to 4.14.

--- autogen.sh
+++ autogen.sh
@@ -1,25 +1,37 @@
 #!/bin/sh
 #
-# Copyright (c) 2002-2009
-#         The Xfce development team. All rights reserved.
+# Copyright (c) 2002-2022 The Xfce development team. All rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
 # Written for Xfce by Benedikt Meurer <benny@xfce.org>
 #                 and Brian Tarricone <brian@tarricone.org>.
 #
 
-export XDT_AUTOGEN_REQUIRED_VERSION="4.9.0"
+export XDT_AUTOGEN_REQUIRED_VERSION="4.14.0"
 
 (type xdt-autogen) >/dev/null 2>&1 || {
   cat >&2 <<EOF
-autogen.sh: You don't seem to have the Xfce development tools (at least
-            version $XDT_REQURED_VERSION) installed on your system, which
-            are required to build this software.
-            Please install the xfce4-dev-tools package first; it is available
-            from https://www.xfce.org/.
+autogen.sh: You don't seem to have the Xfce development tools installed on
+            your system, which are required to build this software.
+            Please install the xfce4-dev-tools package first, available from
+            https://xfce.org/.
 EOF
   exit 1
 }
 
-exec xdt-autogen "$@"
+test -d m4 || mkdir m4
 
-# vi:set ts=2 sw=2 et ai:
+xdt-autogen $@
